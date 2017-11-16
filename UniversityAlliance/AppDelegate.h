5f751d19471b95f4d1fe18930dcb884bff9b4199
徐俊峰 <xujunfeng@lppz.com>
2017-11-16 11:04:01 +0800

    工程递交

---
+++ UniversityAlliance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UniversityAlliance
+//
+//  Created by 徐俊峰 on 2017/11/16.
+//  Copyright © 2017年 XuJunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UniversityAlliance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UniversityAlliance
+//
+//  Created by 徐俊峰 on 2017/11/16.
+//  Copyright © 2017年 XuJunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
